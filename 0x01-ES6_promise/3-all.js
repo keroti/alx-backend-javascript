8f8dcd69e9cb684cf01aa683e0c1093e7aba9aa2
keroti <peterkeroti55@gmail.com>
2023-04-20 00:42:21 +0300

    Handle multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from '../utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
@@ -8,4 +8,3 @@ export default function handleProfileSignup() {
 
     .catch(() => console.log('Signup system offline'));
 }
-
