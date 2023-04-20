ce3343a59fae1cdea20048b16b0b7ef3913a889d
keroti <peterkeroti55@gmail.com>
2023-04-21 01:32:46 +0300

    Methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,37 +1,37 @@
 export default class Currency {
-    constructor(code, name) {
-        if (typeof code !== "string") {
-            throw new TypeError("Name must be a string");
-        }
-        else if (typeof name !== "string") {
-            throw new TypeError("Name must be a string");
-        }
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    get code() {
-        return this._code;
-    }
+    this._code = code;
+    this._name = name;
+  }
 
-    set code(value) {
-        if (typeof value !== "string") {
-            throw new TypeError("Name must be a string");
-        }
-        this._code = value;
-    }
+  get code() {
+    return this._code;
+  }
 
-    get name() {
-        return this._name;
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._code = value;
+  }
 
-    set name(value) {
-        if (typeof value !== "string") {
-            throw new TypeError("Name must be a string");
-        }
-        this._name = value;
-    }
+  get name() {
+    return this._name;
+  }
 
-    displayFullCurrency() {
-        return `${this._name}(${this._code})`
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
