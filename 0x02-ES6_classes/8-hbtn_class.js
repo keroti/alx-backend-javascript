6fa93e4a1f1c6ce0966dd7c01cb19f49cbfca110
keroti <peterkeroti55@gmail.com>
2023-04-21 01:44:06 +0300

    Primitive

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](size) {
+    if (size === 'number') {
+      return this._size;
+    }
+
+    return this._location;
+  }
+}
