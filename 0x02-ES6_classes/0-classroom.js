ecc46ebdc5f5698865dc51dbaa53a4fc21d14c26
keroti <peterkeroti55@gmail.com>
2023-04-20 13:21:08 +0300

    First class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,6 @@
 export default class ClassRoom {
-  _maxStudentsSize;
-
   constructor(maxStudentsSize) {
     // eslint-disable-next-line
-    this._maxStudentsSize = maxStudentsSize;
-  }
-
-  get maxStudentsSize() {
-    return this.maxStudentsSize;
+        this._maxStudentsSize = maxStudentsSize;
   }
 }
