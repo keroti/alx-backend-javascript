1ecd10ac0e21dd52c476146b7a0fec106d326da8
keroti <peterkeroti55@gmail.com>
2023-04-26 01:17:12 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,5 @@
 export default function cleanSet(set, startString) {
   const matchingValues = [];
-
-  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
-    return '';
-  }
-
   //   eslint-disable-next-line
     for (const value of set) {
     if (value && value.startsWith(startString)) {
