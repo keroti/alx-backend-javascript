9d0ba4411314911020aa372dc85514829ea7be90
keroti <peterkeroti55@gmail.com>
2023-04-26 00:59:50 +0300

    Combine map, filter and reduce

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,8 @@
 export default function updateStudentGradeByCity(listOfStudents, city, newGrades) {
   return listOfStudents
     .filter((student) => student.location === city)
-    .map((student) => {
-      // eslint-disable-next-line
-      const matchingGrade = newGrades.reduce((result, grade) => (grade.studentId === student.id ? grade.grade : result), null);
-      return { ...student, grade: matchingGrade ?? 'N/A' };
-    });
+    .map((student) => ({
+      ...student,
+      grade: newGrades.reduce((grade, newGrade) => (newGrade.studentId === student.id ? newGrade.grade : grade), 'N/A'),
+    }));
 }
