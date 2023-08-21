4aa653e69794da4d50b047afd65d79f888456246
keroti <peterkeroti55@gmail.com>
2023-08-22 01:45:09 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,16 +6,22 @@ function countStudents(path) {
             if (error) {
                 reject(new Error("Cannot load the database"));
             } else {
-                const lines = data.split('\n').filter(line => line.trim() !== ''); // Split lines and remove empty lines
+                const lines = data
+                    .split("\n")
+                    .filter((line) => line.trim() !== ""); // Split lines and remove empty lines
                 const fields = {};
                 for (const line of lines) {
                     const [firstName, , , field] = line.split(",");
+                    if (field === "field") {
+                        // Skip the header line
+                        continue;
+                    }
                     if (!fields[field]) {
                         fields[field] = [];
                     }
                     fields[field].push(firstName);
                 }
-                console.log(`Number of students: ${lines.length}`);
+                console.log(`Number of students: ${lines.length - 1}`); // Exclude the header line
                 for (const [field, students] of Object.entries(fields)) {
                     console.log(
                         `Number of students in ${field}: ${
