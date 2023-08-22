4b20affac6f02c51511cdf045fd507a16d7a9eff
keroti <peterkeroti55@gmail.com>
2023-08-22 03:45:21 +0300

    Create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const port = 1245;
 
-app.get("/", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.send("Hello Holberton School!");
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port);
