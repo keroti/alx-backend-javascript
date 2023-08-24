82a8f286af0fc7dd00b4665800059e04c911af1a
keroti <peterkeroti55@gmail.com>
2023-08-25 02:35:51 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,6 +1,22 @@
 const request = require("request");
 const { expect } = require("chai");
 
+describe("Index page", function () {
+    it("should return correct status code", function (done) {
+        request("http://localhost:7865", function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it("should return correct result", function (done) {
+        request("http://localhost:7865", function (error, response, body) {
+            expect(body).to.equal("Welcome to the payment system");
+            done();
+        });
+    });
+});
+
 describe("Regex integration testing", () => {
     describe("GET /cart/:id", () => {
         it("should return Payment methods for cart :id when :id is a number", (done) => {
