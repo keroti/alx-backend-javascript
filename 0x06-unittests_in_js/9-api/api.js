6fae9e963aa71f0afffca48bbdf05a6a50b5d423
keroti <peterkeroti55@gmail.com>
2023-08-25 02:28:17 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 7865;
 
+app.get("/", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.send("Welcome to the payment system");
+});
+
 app.get("/cart/:id(\\d+)", (req, res) => {
     const cartId = req.params.id;
     res.send(`Payment methods for cart ${cartId}`);
